ce7868f05c7ea90aa2a701701977739ccadbeeb5
Touil  Belkacem <belkacemtouil99@gmail.com>
2023-07-02 13:47:59 +0100

    front end for sourat
    - only the front part of it
    
    TODO :
    -  fix some of front end looking
    -NavBar

--- lib/Pages/Home.dart
+++ lib/Pages/Home.dart
@@ -199,8 +199,124 @@ class _HomeState extends State<Home> {
               height: 15,
             ),
             LastRead(),
+            SouraWidget(),
+            SouraWidget(),
+            SouraWidget(),
+            SouraWidget(),
           ],
         ));
+  }
+}
+
+class SouraWidget extends StatelessWidget {
+  const SouraWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            color: Color(0xFF7B80AD),
+            width: 1.0,
+          ),
+        ),
+      ),
+      width: 370,
+      height: 62,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Stack(
+                alignment: Alignment.center,
+                children: [
+                  Image.asset(
+                    'assets/Images/Star.png',
+                    width: 60,
+                    height: 60,
+                  ),
+                  Text(
+                    '1',
+                    style: TextStyle(
+                      fontFamily: 'Poppins',
+                      fontSize: 14,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Al-Fatiah',
+                    style: TextStyle(
+                      fontFamily: 'Poppins',
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        'Meccan',
+                        style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontSize: 13,
+                          color: Color(0xFFA19CC5),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Icon(
+                        Icons.circle,
+                        color: Color(0xFFBBC4CE),
+                        size: 4,
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Text(
+                        '7 verses',
+                        style: TextStyle(
+                          fontSize: 13,
+                          fontFamily: 'Poppins',
+                          color: Color(0xFFA19CC5),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      'الفاتحة',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xFFA44AFF),
+                        fontFamily: 'Amiri',
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
 

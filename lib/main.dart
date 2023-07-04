0b21ccf2af882688a834773c30f3c2c08b0cbbb0
Latif_Kbr <abdelatif4614@gmail.com>
2023-07-04 13:35:56 +0200

    added structure to the project and navigationbar, it still needs functionality, all it does is swap pages.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:quran_app/Pages/Home.dart';
+import 'package:quran_app/Pages/home.dart';
 
-void main() => runApp(MaterialApp(
-      initialRoute: "/",
-      routes: {
-        '/': (context) => const Home(),
-      },
-    ));
+void main() => runApp(
+      MaterialApp(
+        initialRoute: "/",
+        routes: {
+          '/': (context) => const Home(),
+        },
+      ),
+    );
